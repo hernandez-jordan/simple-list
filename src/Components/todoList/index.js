9ed30960df3938a618e73c3636281f78fd161fd0
Jordan Hernandez <Jordan@Jordan.local>
2020-07-18 13:53:35 +0200

    Fix : de-structured props

--- src/Components/todoList/index.js
+++ src/Components/todoList/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const TodoList = props => {
+const TodoList = ({itemChecked, items}) => {
   return (
     <section className="task-list">
       <h2>To do list</h2>
       <ul>
-        {props.items.map((item, key) => (
+        {items.map((item, key) => (
           <div className="list-item" key={key}>
-            <input type={'text'} value={(item.text + '-' + item.id)} readOnly /> 
+            <input type="text" value={(item.text + '-' + item.id)} readOnly /> 
             <input 
               type="checkbox" 
               checked={false}
-              onClick={() => props.itemChecked(item.id, item.text)} 
+              onClick={() => itemChecked(item.id, item.text)} 
               readOnly
               className="checkbox"
             />
